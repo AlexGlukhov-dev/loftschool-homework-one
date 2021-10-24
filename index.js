500eaf675908737723f1a73673910a1f4b926040
Alex <alexglukhov37@gmail.com>
2021-10-24 11:20:00 +0300

    переделал на async - await

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 const path = require('path');
-const util = require('util');
-const fs = require('fs');
+const fs = require('fs').promises;
+
+const outputPath = process.argv[2];
+const exampleFolder = path.join(__dirname, outputPath); //переменная пути к исходной папке
+
+console.log(fs.readdir(exampleFolder));
+
+(async () => {
+    try{
+        const data = await fs.readdir(exampleFolder);
 
 
+        const arFiles = data.toString().split(','); // создаем маccив имени файлов
+
+        let folderNames = []; //инициализируем пустой массив в котором будут храниться имена папок которые нужно создать
+
+        arFiles.map(f => {
+            folderNames.push(f[0]);
+        })
+
+        let i = 0;
+
+        for (const folder of folderNames) {
+            let upperFold = folder.toUpperCase(); //переводим имя папки в большой регистр
+            let newFolder = path.join(__dirname, 'result', upperFold); // создаем путь до конечных папок
+            let file = path.join(__dirname, outputPath, arFiles[i]); // создаем путь до файла
+
+            await fs.mkdir(path.join(newFolder), {recursive: true}); // создаем папку если ее нет
 
 
-const exampleFolder = path.join(__dirname, process.argv[2]); //переменная пути к исходной папке
-
-fs.readdir(exampleFolder, (err, data) =>{
-    if(err){
-        console.log(err);
-        return;
+            if (folder === arFiles[i][0]) { // делаем проверку какая картинка должна оказаться в какой папке
+                await fs.rename(file, `${newFolder}/${arFiles[i]}`);
+            }
+            i++;
+        }
+        await fs.rmdir(exampleFolder);
+    }catch (e){
+        console.log(e);
     }
-
-    const arFiles = data.toString().split(','); // создаем маccив имени файлов
-
-    let folderNames = []; //инициализируем пустой массив в котором будут храниться имена папок которые нужно создать
-
-    arFiles.map(f =>{
-        folderNames.push(f[0]);
-    })
-
-    let i = 0;
-
-    folderNames.forEach(folder => {
-        let upperFold = folder.toUpperCase(); //переводим имя папки в большой регистр
-            newFolder = path.join(__dirname,'result',upperFold); // создаем путь до конечных папок
-            file = path.join(__dirname, process.argv[2], arFiles[i]); // создаем путь до файла
-
-        if(!fs.existsSync(newFolder)) { //проверяем наличие папки
-            fs.mkdirSync(path.join(newFolder), { recursive: true }); // создаем папку если ее нет
-        }
-
-        if(folder === arFiles[i][0]){ // делаем проверку какая картинка должна оказаться в какой папке
-            (async () => {
-                await fs.rename(file, `${newFolder}/${arFiles[i]}`, err => {
-                    if(err){
-                        console.log(err);
-                    }
-                });
-            })();
-        }
-        i++;
-    })
-    fs.rmdir(exampleFolder, err => { //удаление исходной папки
-        if(err){
-            console.log(err);
-        }
-    })    
-});
+})();
